5a02dd13d4cd93eaf2fb71a0d0f69ee4bb92ee57
alvaroormeno <alvaro@ormeno.org>
2022-08-05 00:40:27 -0400

    created schema file, imported to index.js, set up graphiql to true for development

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
 
 const port = process.env.PORT || 5001
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use('/graphql', graphqlHTTP({
     // Require schema
+    schema,
+    // set to true only when in development
+    graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`SERVER RUNNING ON PORT ${port}`))
