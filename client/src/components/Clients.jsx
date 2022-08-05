44376f47ac7849b7a29b86fc1e7250e5b9f94860
alvaroormeno <alvaro@ormeno.org>
2022-08-05 17:57:43 -0400

    in cliens component, used gql query momentarily to query the clients from db, added table and inside map each client to render clientrow component

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import {gql, useQuery} from '@apollo/client'
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients{
+      name
+      email
+      phone
+    }
+  }
+`
 
 const Clients = () => {
+
+  const { loading, error, data } = useQuery(GET_CLIENTS)
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Something Went Wrong</p>
+
   return (
-    <div>Clients</div>
+    <>
+      {!loading && !error && (
+        <table className='table table-hover mt-3'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            // map for each client from data to render a clientrow component wich we are passing the key and the client data.
+            {data.clients.map(client => (
+              <ClientRow key={client.id} client={client}/>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   )
 }
 
