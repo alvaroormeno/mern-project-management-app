b5892b77104b345f0cae700cf703c1d1d8be57d5
alvaroormeno <alvaro@ormeno.org>
2022-08-05 00:56:56 -0400

    initial schema.js setup

---
+++ server/schema/schema.js
@@ -0,0 +1,10 @@
+// destructure and bring projects and clients to work with
+const {projects, clients} = require('../sampleData.js')
+// destructure graphql to bring specific graphql functions/properties
+const { GraphQLObjectType} = require('graphql')
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+name: 'Client',
+
+})
