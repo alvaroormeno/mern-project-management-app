c111e97f0a39f8f9e6ca1621d59da4754dcb2e94
alvaroormeno <alvaro@ormeno.org>
2022-08-05 17:25:12 -0400

    declared apollo client and wrapped it around app.js return

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import Header from "./components/Header";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-    
-    <Header/>
-    <div className="Container">
-      <h1>hello world</h1>
-    </div>
-
+    <ApolloProvider client={client}>
+      <Header/>
+      <div className="Container">
+        <h1>hello world</h1>
+      </div>
+    </ApolloProvider>
     </>
   );
 }
