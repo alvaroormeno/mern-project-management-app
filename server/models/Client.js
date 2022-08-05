6e791f5273905df8b946f5e9ad042889f05c2d7d
alvaroormeno <alvaro@ormeno.org>
2022-08-05 12:44:16 -0400

    added .env to gitignore

---
+++ server/models/Client.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+
+// Important! - mongoose schema is not related to graphql schema. Ex. First comes mongo database, on top we have mongoose layer where we create schemas that includes fields for our db collections, on top we have the GraphQl api where our graphql schemas come in.
