bb10d20a69f2d9dcb9bc94e2c000609c10f24736
alvaroormeno <alvaro@ormeno.org>
2022-08-05 10:30:52 -0400

    declared project type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,18 @@ const {projects, clients} = require('../sampleData.js')
 // destructure graphql to bring specific graphql functions/properties
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
 
+
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    description: {type: GraphQLString},
+    status: {type: GraphQLString}
+  }) 
+})
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -14,16 +26,18 @@ const ClientType = new GraphQLObjectType({
   }) 
 })
 
-//Root Query - to query a client by their id
+//Root Query - 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // query to get all clients
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
         return clients;
       }
     },
+    // query to get client by id
     client: {
       type: ClientType,
       args: {id: {type: GraphQLID}},
