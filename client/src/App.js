8fa5629019c5d619e427f02e305a0b1608a96799
alvaroormeno <alvaro@ormeno.org>
2022-08-05 17:21:18 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
+    
+    <Header/>
     <div className="Container">
       <h1>hello world</h1>
     </div>
+
+    </>
   );
 }
 
