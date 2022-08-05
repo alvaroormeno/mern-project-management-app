f919f073015a19824d160be4221fe9f3379562dc
alvaroormeno <alvaro@ormeno.org>
2022-08-05 13:54:01 -0400

    declared client mongoose schema

--- server/models/Client.js
+++ server/models/Client.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose')
 
-// Important! - mongoose schema is not related to graphql schema. Ex. First comes mongo database, on top we have mongoose layer where we create schemas that includes fields for our db collections, on top we have the GraphQl api where our graphql schemas come in. test
+// Important! - mongoose schema is not related to graphql schema. Ex. First comes mongo database, on top we have mongoose layer where we create schemas that includes fields for our db collections, on top we have the GraphQl api where our graphql schemas come in.
+const ClientSchema = new mongoose.Schema({
+  // object of fields that we want for schema
+  name:{
+    type: String,
+  }, 
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+// Export as mongoose model (name, schema)
+module.exports = mongoose.model('Client', ClientSchema) 
