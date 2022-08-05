07850d8646e7ebe18d7262cbb719b962e0639a47
alvaroormeno <alvaro@ormeno.org>
2022-08-05 00:37:06 -0400

    required express-graphql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 require('dotenv').config();
+const {graphqlHTTP} = require('express-graphql')
 
 const port = process.env.PORT || 5001
 
-
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    // Require schema
+}))
 
 app.listen(port, console.log(`SERVER RUNNING ON PORT ${port}`))
