42328ddbc24677ee797314e2231312bea3407e10
alvaroormeno <alvaro@ormeno.org>
2022-08-06 13:47:18 -0400

    created new add_project mutation, on addproject modal passed clients info to be selected when adding project. All working and rendering

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { FaList } from "react-icons/fa";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_PROJECTS } from "../queries/projectQueries";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { ADD_PROJECT } from "../mutations/projectMutations";
+import Spinner from "./Spinner";
 
 
 
@@ -13,42 +16,50 @@ const AddClientModal = () => {
   const [clientId, setClientId] = useState("");
   const [status, setStatus] = useState("new");
 
-  // // Use ADD_CLIENT mutation
-  // const [addClient] = useMutation(ADD_CLIENT, {
-  //   variables: {name, email, phone},
-  //   //update cache to render newly added client on page
-  //   update(cache, {data: {addClient}}) {
-  //     // read the cache which query is GET_CLIENTS mutation and destructure to grab clients
-  //     const {clients} = cache.readQuery({ query: GET_CLIENTS});
+  // // Use ADD_PROJECT mutation
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: {name, description, clientId, status},
+    //update cache to render newly added project on page
+    update(cache, {data: {addProject}}) {
+      // read the cache which query is GET_PROJECT mutation and destructure to grab projects
+      const {projects} = cache.readQuery({ query: GET_PROJECTS});
 
-  //     cache.writeQuery({
-  //       query: GET_CLIENTS,
-  //       data: { clients: [...clients, addClient]},
-  //     })
-  //   }
-  // })
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject]},
+      })
+    }
+  })
+
+  // Get client for selecting client
+  const {loading, error, data} = useQuery(GET_CLIENTS);
+
 
 
   const onSubmit = (e) => {
     e.preventDefault();
-
     //validate that inputs have name email and phone by checking if states are empty
     if(name === '' || description === '' || status === '') {
       return alert('Please fill in all 3 fields to continue');
-    }
-    
-
+    };
+    //call addProject function and pass input state values to use for ADD_PROJECT mutation
+    addProject(name, description, clientId, status)
     //clear states therefore clear inputs
     setName("");
     setDescription("");
     setStatus("new");
-    
+    setClientId("")
+  };
 
-  }
+  if(loading) return null;
+  if (error) return 'somethign went wrong!'
 
-	return (
+	return ( 
 		<>
-			<button
+    {/* only if its not loading and no error we want to render the modal, because we want the GET_CLIENTS query to load first to have clients info available before modal renders. */}
+    {!loading && !error && (
+       <>
+       <button
 				type="button"
 				className="btn btn-primary"
 				data-bs-toggle="modal"
@@ -105,13 +116,27 @@ const AddClientModal = () => {
 								</div>
                 {/* STATUS */}
 								<div className="mb-3">
-									<label className="form-label">Phone</label>
+									<label className="form-label">Status</label>
 									<select className="form-select"  id="status" value={status} onChange={(e) => setStatus(e.target.value)}>
                     <option value="new">Not Started</option>
                     <option value="progress">In Progress</option>
                     <option value="completed">Completed</option>
                   </select>
 								</div>
+                {/* CLIENT SELECT */}
+                <div className="mb-3">
+                  <label className="form-label">Client</label>
+                  <select id="client" className="form-select" value={clientId} onChange={(e) => setClientId(e.target.value)}>
+                    <option value="">SelectClient</option>
+                    {data.clients.map((client) => (
+                      <option key={client.id} value={client.id}>
+                        {client.name}
+                      </option> ))}
+                  </select>
+                  
+                  
+
+                </div>
 
                 <button type="submit" className="btn btn-primary" data-bs-dismiss="modal" >
                   Submit
@@ -121,6 +146,9 @@ const AddClientModal = () => {
 					</div>
 				</div>
 			</div>
+       </>
+    )}
+			
 		</>
 	);
 };
