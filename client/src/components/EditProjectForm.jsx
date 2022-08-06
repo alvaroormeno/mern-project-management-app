92353b1a8e95b88f084510b51e594011ae70348a
alvaroormeno <alvaro@ormeno.org>
2022-08-06 14:58:07 -0400

    created UPDATE_PROJECT mutation, added update project fields inside project page, updating project works and renders changes

---
+++ client/src/components/EditProjectForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { GET_PROJECT } from "../queries/projectQueries";
+import { UPDATE_PROJECT } from "../mutations/projectMutations";
+
+const EditProjectForm = ({project}) => {
+
+  const [name, setName] = useState(project.name);
+  const [description, setDescription] = useState(project.description);
+  const [status, setStatus] = useState('');
+
+  const [updateProject] = useMutation(UPDATE_PROJECT, {
+    variables: { id: project.id, name, description, status},
+    //refetching GET_PROJECT query to update the project on page
+    refetchQueries: [{query: GET_PROJECT, variables: {id: project.id}}],
+  })
+
+  const onSubmit = (e) => {
+
+    //validation
+    if(!name || !description || !status ) {
+      return alert(" Please fill out all fields")
+    }
+
+    updateProject(name, description, status)
+
+  }
+  
+
+    
+	return (
+		<div className="mt-5">
+			<h3>Update Project Details</h3>
+			<form onSubmit={onSubmit}>
+				{/* NAME */}
+				<div className="mb-3">
+					<label className="form-label">Name</label>
+					<input
+						type="text"
+						className="form-control"
+						id="name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				{/* DESCRIPTION */}
+				<div className="mb-3">
+					<label className="form-label">Description</label>
+					<textarea
+						type="email"
+						className="form-control"
+						id="description"
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+					></textarea>
+				</div>
+				{/* STATUS */}
+				<div className="mb-3">
+					<label className="form-label">Status</label>
+					<select
+						className="form-select"
+						id="status"
+						value={status}
+						onChange={(e) => setStatus(e.target.value)}
+					>
+						<option value="new">Not Started</option>
+						<option value="progress">In Progress</option>
+						<option value="completed">Completed</option>
+					</select>
+				</div>
+
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+			</form>
+		</div>
+	);
+};
+
+export default EditProjectForm;
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client'
 import { GET_PROJECT } from '../queries/projectQueries'
 import ClientInfo from '../components/ClientInfo'
 import DeleteProjectButton from '../components/DeleteProjectButton'
+import EditProjectForm from '../components/EditProjectForm'
 
 const Project = () => {
 
@@ -34,6 +35,8 @@ const Project = () => {
           <p className='lead'>{data.project.status}</p>
           
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
 
           <DeleteProjectButton projectId={data.project.id} />
         </div>
