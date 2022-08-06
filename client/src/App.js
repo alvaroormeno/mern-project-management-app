fb3841f45f7c77d3f60a5a4ffc8e3db0af181fa2
alvaroormeno <alvaro@ormeno.org>
2022-08-06 00:19:06 -0400

    projects are now rendering on home page, created query for GET_PROJECTS and imported it into projects.jsx where we map a projectcard.jsx component which renders each project and their info

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
 
 // created so cache data maybe be lost warning stops appearing. Merging old with new cache
 const cache = new InMemoryCache({
@@ -36,6 +37,7 @@ function App() {
       <Header/>
       <div className="Container">
         <AddClientModal/>
+        <Projects/>
         <Clients></Clients>
       </div>
     </ApolloProvider>
